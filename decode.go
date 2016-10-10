1b61f1f976d5cc678ccb5c9b1476e583f8aa0d18
Gregory McDaniel <Gregory.McDaniel@infor.com>
2016-10-10 20:14:05 +0100

    fail_on_unspecified_fields Add a strict mode to Decode.

--- decode.go
+++ decode.go
@@ -186,11 +186,31 @@ func (p *parser) mapping() *node {
 // ----------------------------------------------------------------------------
 // Decoder, unmarshals a node into a provided value.
 
+// Decoder unmarshals a node into a provided value.
+type Decoder struct {
+	*decoder
+}
+
+// NewDecoder creates and initializes a new Decoder struct.
+func NewDecoder() *Decoder {
+	return &Decoder{
+		newDecoder(),
+	}
+}
+
+// WithStrict sets strict mode on the Decoder struct. When enabled
+// any unspecified fields in the yaml document will through an error.
+func (d *Decoder) WithStrict(strict bool) *Decoder {
+	d.strict = strict
+	return d
+}
+
 type decoder struct {
 	doc     *node
 	aliases map[string]bool
 	mapType reflect.Type
 	terrors []string
+	strict  bool
 }
 
 var (
@@ -640,6 +660,9 @@ func (d *decoder) mappingStruct(n *node, out reflect.Value) (good bool) {
 			value := reflect.New(elemType).Elem()
 			d.unmarshal(n.children[i+1], value)
 			inlineMap.SetMapIndex(name, value)
+		} else if d.strict {
+			d.terrors = append(d.terrors, fmt.Sprintf("line %d: no such field '%s' in struct '%s'", ni.line+1, name, out.Type()))
+			return false
 		}
 	}
 	return true
--- yaml.go
+++ yaml.go
@@ -76,9 +76,51 @@ type Marshaler interface {
 // See the documentation of Marshal for the format of tags and a list of
 // supported tag options.
 //
-func Unmarshal(in []byte, out interface{}) (err error) {
+func Unmarshal(in []byte, out interface{}) error {
+	return unmarshal(newDecoder(), in, out)
+}
+
+// Unmarshal decodes the first document found within the in byte slice
+// and assigns decoded values into the out value.
+//
+// Maps and pointers (to a struct, string, int, etc) are accepted as out
+// values. If an internal pointer within a struct is not initialized,
+// the yaml package will initialize it if necessary for unmarshalling
+// the provided data. The out parameter must not be nil.
+//
+// The type of the decoded values should be compatible with the respective
+// values in out. If one or more values cannot be decoded due to a type
+// mismatches, decoding continues partially until the end of the YAML
+// content, and a *yaml.TypeError is returned with details for all
+// missed values.
+//
+// Struct fields are only unmarshalled if they are exported (have an
+// upper case first letter), and are unmarshalled using the field name
+// lowercased as the default key. Custom keys may be defined via the
+// "yaml" name in the field tag: the content preceding the first comma
+// is used as the key, and the following comma-separated options are
+// used to tweak the marshalling process (see Marshal).
+// Conflicting names result in a runtime error.
+//
+// For example:
+//
+//     type T struct {
+//         F int `yaml:"a,omitempty"`
+//         B int
+//     }
+//     var t T
+//     d := yam.NewDecoder()
+//     d.Unmarshal([]byte("a: 1\nb: 2"), &t)
+//
+// See the documentation of Marshal for the format of tags and a list of
+// supported tag options.
+//
+func (d *Decoder) Unmarshal(in []byte, out interface{}) error {
+	return unmarshal(d.decoder, in, out)
+}
+
+func unmarshal(d *decoder, in []byte, out interface{}) (err error) {
 	defer handleErr(&err)
-	d := newDecoder()
 	p := newParser(in)
 	defer p.destroy()
 	node := p.parse()

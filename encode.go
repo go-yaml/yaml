716c1331f01bc17b06d3ab65dd7e6c555689a08b
Roger Peppe <rogpeppe@gmail.com>
2017-07-21 17:31:03 +0100

    Fixes to timestamp support.
    Factor the timestamp parsing into its own function that
    can easily be changed later to support the full range of
    time stamps and add a quick early check to avoid
    going through all the time.Parse calls in this common
    path.
    
    Also fix marshaling so that times are still printed unquoted.

--- encode.go
+++ encode.go
@@ -251,11 +251,17 @@ func (e *encoder) stringv(tag string, in reflect.Value) {
 			failf("cannot marshal invalid UTF-8 data as %s", shortTag(tag))
 		}
 	}
-	if tag == "" && (rtag != yaml_STR_TAG || isBase60Float(s)) {
+	switch {
+	case rtag == yaml_TIMESTAMP_TAG:
+		// TODO with the current code, there's no way for tag to be non-empty,
+		// but what should this function do if (for example) tag is yaml_BOOL_TAG
+		// and rtag is something incompatible with it?
+		style = yaml_PLAIN_SCALAR_STYLE
+	case tag == "" && (rtag != yaml_STR_TAG || isBase60Float(s)):
 		style = yaml_DOUBLE_QUOTED_SCALAR_STYLE
-	} else if strings.Contains(s, "\n") {
+	case strings.Contains(s, "\n"):
 		style = yaml_LITERAL_SCALAR_STYLE
-	} else {
+	default:
 		style = yaml_PLAIN_SCALAR_STYLE
 	}
 	e.emitScalar(s, "", tag, style)

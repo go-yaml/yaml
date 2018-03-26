e47eca576e8f3a433de0ba77f1923e7c7f959667
Sebastian Vidrio <svidrio@gmail.com>
2018-03-25 21:56:21 -0700

    increment non-zero scanner error lines (#319)
    Scanner errors are 0 indexed. Errors occuring on lines greater than 0, referenced the previous line in the error text.
    
    Fixes #205.

--- decode_test.go
+++ decode_test.go
@@ -825,6 +825,8 @@ var unmarshalErrorTests = []struct {
 	{"v: !!float 'error'", "yaml: cannot decode !!str `error` as a !!float"},
 	{"v: [A,", "yaml: line 1: did not find expected node content"},
 	{"v:\n- [A,", "yaml: line 2: did not find expected node content"},
+	{"a:\n- b: *,", "yaml: line 2: did not find expected alphabetic or numeric character"},
+	{"a:\n- {b: c, d:e},", "yaml: line 2: found unexpected ':'"},
 	{"a: *b\n", "yaml: unknown anchor 'b' referenced"},
 	{"a: &a\n  b: *a\n", "yaml: anchor 'a' value contains itself"},
 	{"value: -", "yaml: block sequence entries are not allowed in this context"},

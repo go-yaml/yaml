fa04dc4e40640256da83bfb2f49b928b1385db8f
Alfred Krohmer <alfred.krohmer@goto.com>
2022-04-06 10:39:41 +0200

    fix: recognize escaped forward slash

--- scannerc.go
+++ scannerc.go
@@ -2523,6 +2523,8 @@ func yaml_parser_scan_flow_scalar(parser *yaml_parser_t, token *yaml_token_t, si
 					s = append(s, '\'')
 				case '\\':
 					s = append(s, '\\')
+				case '/':
+					s = append(s, '/')
 				case 'N': // NEL (#x85)
 					s = append(s, '\xC2')
 					s = append(s, '\x85')
